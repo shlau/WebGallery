ead1de793ba3f3ed17659954f6edf9e00e31b4bc
sheldon <sheldonlau1@gmail.com>
2017-09-04 16:10:24 -0700

    Setup Spring with Hibernate,h2, and Thymeleaf

---
+++ src/main/java/com/sheldon/gallery/web/Controller/GreetingController.java
@@ -0,0 +1,16 @@
+package com.sheldon.gallery.web.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String greeting() {
+        return "hello!!!!";
+    }
+
+}
